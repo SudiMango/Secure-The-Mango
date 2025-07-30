71540a6a127f2ca9894115626fe0fcb63d5693f0
SudiMango <sudipto.islam2005@gmail.com>
2025-07-30 16:47:06 +0600

    Added dashing and cleaned code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,36 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Public variables
 
-    public Rigidbody2D rb;
-    public float moveSpeed = 5f;
-    public float jumpForce = 5f;
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
 
-    // Private variables
+    [Header("Movement settings")]
+    [SerializeField] private float moveSpeed = 5f;
+    private Vector2 moveDir = Vector2.zero;
 
+    [Header("Jump settings")]
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float doubleJumpMultiplier = 0.7f;
+    private bool isGrounded = true;
+    private bool canDoubleJump = false;
+
+
+    [Header("Dash settings")]
+    [SerializeField] private float dashForce = 10f;
+    [SerializeField] private float dashTime = 0.3f;
+    [SerializeField] private float dashCooldown = 1f;
+    private bool canDash = true;
+    private bool isDashing = false;
+
+    // Player controls
     private PlayerInputActions playerControls;
     private InputAction move;
     private InputAction jump;
-
-    private Vector2 moveDir = Vector2.zero;
+    private InputAction dash;
 
     void Awake()
     {
@@ -27,11 +42,14 @@ public class PlayerController : MonoBehaviour
     {
         move = playerControls.Player.Move;
         jump = playerControls.Player.Jump;
+        dash = playerControls.Player.Dash;
 
         move.Enable();
         jump.Enable();
+        dash.Enable();
 
         jump.performed += onJump;
+        dash.performed += onDash;
     }
 
     // Disable player input systems
@@ -39,6 +57,7 @@ public class PlayerController : MonoBehaviour
     {
         move.Disable();
         jump.Disable();
+        dash.Disable();
     }
 
     // Update is called once per frame
@@ -49,13 +68,77 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (isDashing) return;
+
         rb.linearVelocityX = moveDir.x * moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            isGrounded = false;
+        }
     }
 
     // MODIFIES: rb
     // EFFECTS: makes the player jump when jump input action is performed
     private void onJump(InputAction.CallbackContext context)
     {
-        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        if (isDashing) return;
+
+        if (isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            canDoubleJump = true;
+        }
+        else if (!isGrounded && canDoubleJump)
+        {
+            rb.AddForce(Vector2.up * jumpForce * doubleJumpMultiplier, ForceMode2D.Impulse);
+            canDoubleJump = false;
+        }
+    }
+
+    // MODIFIES: rb
+    // EFFECTS: makes the player dash when dash input action is performed
+    private void onDash(InputAction.CallbackContext context)
+    {
+        if (!canDash) return;
+
+        StartCoroutine(Dash());
+    }
+
+    // MODIFIES: self, rb
+    // EFFECTS: performs dash action
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        rb.linearVelocity = new Vector2(getDashDir() * dashForce, 0);
+        float prevGravityScale = rb.gravityScale;
+        rb.gravityScale = 0;
+        yield return new WaitForSeconds(dashTime);
+        isDashing = false;
+        rb.gravityScale = prevGravityScale;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+
+    // EFFECTS: returns the dash direction based on player's x localscale
+    private int getDashDir()
+    {
+        if (gameObject.transform.localScale.x < 0)
+        {
+            return -1;
+        }
+        return 1;
     }
 }

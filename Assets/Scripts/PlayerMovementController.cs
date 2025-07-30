4ba95c4375d90da097628e0d7291f347cb521338
SudiMango <sudipto.islam2005@gmail.com>
2025-07-30 23:49:31 +0600

    Added wall sliding and cleaned up code

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    #region Variables
 
     [Header("References")]
     [SerializeField] private Rigidbody2D rb;
@@ -26,6 +29,12 @@ public class PlayerController : MonoBehaviour
     private bool canDash = true;
     private bool isDashing = false;
 
+    [Header("Wall sliding settings")]
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+    private bool isWallSliding = false;
+    private float wallSlidingSpeed = 2f;
+
     // Player controls
     private PlayerInputActions playerControls;
     private InputAction move;
@@ -35,6 +44,10 @@ public class PlayerController : MonoBehaviour
     // Other
     Vector3 mousePos = Vector3.zero;
     private bool facingRight = true;
+
+    #endregion
+
+    #region Unity callbacks
 
     void Awake()
     {
@@ -82,6 +95,9 @@ public class PlayerController : MonoBehaviour
         {
             flip();
         }
+
+        // Constantly check for wall slide and do it if applicable
+        wallSlide();
     }
 
     void FixedUpdate()
@@ -91,6 +107,48 @@ public class PlayerController : MonoBehaviour
         // Move character
         rb.linearVelocityX = moveDir.x * moveSpeed;
     }
+
+    #endregion
+
+    #region Custom functions
+
+    #region
+
+    // EFFECTS: return true if player is grounded, otherwise return false
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    // EFFECTS: return true if player is touching a wall, otherwise return false
+    private bool isWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+    // EFFECTS: returns the dash direction based on player's x localscale
+    private int getDashDir()
+    {
+        if (gameObject.transform.localScale.x < 0)
+        {
+            return -1;
+        }
+        return 1;
+    }
+
+    // MODIFIES: transform.localScale
+    // EFFECTS: flips the character on the x-axis
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+        facingRight = !facingRight;
+    }
+
+    #endregion
+
+    #region Movement related functions
 
     // MODIFIES: rb
     // EFFECTS: makes the player jump when jump input action is performed
@@ -119,9 +177,19 @@ public class PlayerController : MonoBehaviour
         StartCoroutine(Dash());
     }
 
-    private bool isGrounded()
+    // MODIFIES: self, rb
+    // EFFECTS: if player can wall slide, then do so
+    private void wallSlide()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        if (isWalled() && !isGrounded() && moveDir.x != 0)
+        {
+            isWallSliding = true;
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, Math.Clamp(rb.linearVelocityY, -wallSlidingSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
     }
 
     // MODIFIES: self, rb
@@ -140,23 +208,7 @@ public class PlayerController : MonoBehaviour
         canDash = true;
     }
 
-    // EFFECTS: returns the dash direction based on player's x localscale
-    private int getDashDir()
-    {
-        if (gameObject.transform.localScale.x < 0)
-        {
-            return -1;
-        }
-        return 1;
-    }
+    #endregion
 
-    // MODIFIES: transform.localScale
-    // EFFECTS: flips the character on the x-axis
-    private void flip()
-    {
-        Vector3 localScale = transform.localScale;
-        localScale.x *= -1;
-        transform.localScale = localScale;
-        facingRight = !facingRight;
-    }
+    #endregion
 }
